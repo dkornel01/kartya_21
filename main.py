756746e005a17f9c9a230cecd974e5eea2c241f1
dkornel01 <kornel.dobrocsi@gmail.com>
2023-02-02 15:30:20 +0100

    kartya

---
+++ main.py
@@ -0,0 +1,10 @@
+def eredmeny():
+
+
+def tesztek_esetek():
+    jatekos_vesztett()
+    tesz_vesztett()
+def jatekos_vesztett():
+    jatekos=[10,9,3]
+    gep=[10,9]
+    vart_eredmeny="jatekos vesztett"
